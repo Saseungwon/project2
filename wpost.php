dde95486e914cc2654b460c2b064908d3d9be579
Saseungwon <dfg1425@naver.com>
2021-04-26 12:50:10 +0900

    project_ffinal

--- wpost.php
+++ wpost.php
@@ -1,12 +1,11 @@
 <?php
-  error_reporting(E_ALL); 
-  ini_set("display_errors", 1);
+  // error_reporting(E_ALL); 
+  // ini_set("display_errors", 1);
 
   //xml을 json으로 변환후 echo 해준다
-    $fileContents = file_get_contents("http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?bgnde=20140301&endde=20140430&pageNo=".$_POST["pageNo"]."&numOfRows=10&ServiceKey=EUnXGRb0BjTBz1zzFA%2FK9O9IAuKxSM6yPXe8MXvBfzrhZ%2Fg05WplooTIPhBm2L1XWImR4uhlUwgX9lSSisWNwQ%3D%3D");
+    $fileContents = file_get_contents("http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic?bgnde=20140301&endde=20140430&pageNo=".$_POST["pageNo"]."&numOfRows=10&ServiceKey=EUnXGRb0BjTBz1zzFA/K9O9IAuKxSM6yPXe8MXvBfzrhZ/g05WplooTIPhBm2L1XWImR4uhlUwgX9lSSisWNwQ==");
     $simpleXml = simplexml_load_string($fileContents);
     $json = json_encode($simpleXml);
     echo $json;
 
 ?>
-
